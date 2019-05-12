4be2915ef935c8e0571a5fbb5d58cfc7305e2a6b
Andrew Martin <Janmebows@gmail.com>
2019-05-12 10:28:13 +0930

    Working fluid rankine code
    Plus some changes to thesis

--- fluidRankineImproved.m
+++ fluidRankineImproved.m
@@ -1,10 +1,12 @@
+close all 
+clear all
 global W k R r0
 W = 1;
 k = 5;
 R = 1;
 r0 = 0.25;
 
-det =@(rhat,rstar) rhat*(bessely(1,k*rhat)*besselj(1,k*rstar) - besselj(1,k*rhat)*bessely(1,k*rhat));
+det =@(rhat,rstar) rhat*(bessely(1,k*rhat)*besselj(1,k*rstar) - besselj(1,k*rhat)*bessely(1,k*rstar));
 A =@(rhat,rstar) 1/det(rhat,rstar) * (rhat*bessely(1,k*rhat)*(-0.5*W*rstar) -bessely(1,k*rstar)*(0.5*W*(r0^2-rhat^2)));
 B =@(rhat,rstar) 1/det(rhat,rstar) * (-rhat*besselj(1,k*rhat)*(-0.5*W*rstar) +besselj(1,k*rstar)*(0.5*W*(r0^2-rhat^2)));
 C =@(rhat,rstar) 0.5*(W + k*(A(rhat,rstar)*besselj(0,k*rhat) + B(rhat,rstar)*bessely(0,k*rhat)));
@@ -14,22 +16,28 @@ rstareqn =@(rhat,rstar) W + k*(A(rhat,rstar)*besselj(0,k*rstar) + B(rhat,rstar)*
 
 x0 = [0.5,0.3];
 lb = [0,0];
-ub = [R,R];
+ub = [0.99,0.99];
 [x,resnorm,residual,flag] = lsqnonlin(@equations,x0,lb,ub);
 rhat = x(1);
 rstar = x(2);
 r= linspace(0,R);
+PsiIn = 0.5*W*rstar.^2 + r.*(A(rhat,rstar) .* besselj(1,k*r) + B(rhat,rstar).*bessely(1,k*r));
+PsiOut = C(rhat,rstar)* r.^2 + D(rhat,rstar);
+Psifull = [PsiIn(r<rhat),PsiOut(r>=rhat)];
+plot(r,Psifull)
+
 Win = W + k*(A(rhat,rstar)*besselj(0,k*r) + B(rhat,rstar)*bessely(0,k*r));
 Wout = ones(1,length(r))*2*C(rhat,rstar);
 
 Wfull = [Win(r<rhat), Wout(r>=rhat)];
 Wfull(r<rstar) = 0;
+figure
 plot(r,Wfull)
 function output = equations(x)
 global W k R r0
 rhat = x(1);
 rstar = x(2);
-det = rhat*(bessely(1,k*rhat)*besselj(1,k*rstar) - besselj(1,k*rhat)*bessely(1,k*rhat));
+det = rhat*(bessely(1,k*rhat)*besselj(1,k*rstar) - besselj(1,k*rhat)*bessely(1,k*rstar));
 A = 1/det * (rhat*bessely(1,k*rhat)*(-0.5*W*rstar) -bessely(1,k*rstar)*(0.5*W*(r0^2-rhat^2)));
 B = 1/det * (-rhat*besselj(1,k*rhat)*(-0.5*W*rstar) +besselj(1,k*rstar)*(0.5*W*(r0^2-rhat^2)));
 C = 0.5*(W + k*(A*besselj(0,k*rhat) + B*bessely(0,k*rhat)));
